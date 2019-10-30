919e12d8122347b5e92bd46017348a8a1f02eeda
v1tal4ik <vetalgta@gmail.com>
2019-10-30 15:40:04 +0200

    fix problem, add test

--- hw1/v1tal4ik.js
+++ hw1/v1tal4ik.js
@@ -1,6 +1,12 @@
 // 1.Write a Javascript program to create a new string which is n copies of a given string where n is a non-negative integer.
 
-function repeatString(str, value) {
+function repeatString(str = '', value = 0) {
+  if ((typeof (str)) !== 'string' || (typeof (value) !== 'number') || value < 0) {
+    const error = 'Error: type of arguments are invalid';
+    console.error(error);
+    return error;
+  }
+
   let basicStr = str;
   for (let i = 1; i < value; i++) {
     basicStr += str;
@@ -8,13 +14,17 @@ function repeatString(str, value) {
   return basicStr;
 }
 
-const repeatStr = repeatString('Hi Tomas!', 4);
-console.log(repeatStr);
+const repeatStr = repeatString('Hi Tomas!', 3);
+console.log({ repeatStr });
 
 
 // 2.Write a Javascript program to check if a string starts with "if".
 
 function checkIfStartsWithIf(str = '') {
+  if (typeof (str) !== 'string') {
+    console.error('Error: Type of argument "str" is invalid');
+    return false;
+  }
   const regExp = /if/;
   const text = str.toLowerCase().trim();
   const info = text.match(regExp);
@@ -27,11 +37,24 @@ function checkIfStartsWithIf(str = '') {
 
 const example = 'if i were sultan, I will have a lots of wife';
 const isStart = checkIfStartsWithIf(example);
-console.log(isStart);
+console.log({ isStart });
+
 
 // 3.Write a Javascript program to check two non-negative integer values and return true if they have the same last digit
 
 function checkLastDigit(num1 = 0, num2 = 0) {
+  if (typeof (num1) !== 'number' || typeof (num2) !== 'number') {
+    const error = 'Error: type of arguments are invalid';
+    console.error(error);
+    return false;
+  }
+
+  if (num1 < 0 || num2 < 0) {
+    const error = 'Error: arguments are negative';
+    console.error(error);
+    return false;
+  }
+
   const textOfNumber1 = num1.toString().split('');
   const textOfNumber2 = num2.toString().split('');
 
@@ -42,13 +65,30 @@ function checkLastDigit(num1 = 0, num2 = 0) {
 }
 
 const isEqual = checkLastDigit(123, 223);
-console.log(isEqual);
+console.log({ isEqual });
+
 
 // 4. Information of number
 
 function getSomeInfoOfNumber(number = 0) {
+  if (typeof (number) !== 'number') {
+    console.log('Error: type of argument must be Number');
+    return { message: 'Error: type of argument must be Number' };
+  }
+  if (number <= 0) {
+    console.log('Error: argument must be positive number');
+    return { message: 'Error: argument must be positive number' };
+  }
+  if (number % 1 !== 0) {
+    console.log('Error: argument must be integer');
+    return { message: 'Error: argument must be integer' };
+  }
+  if (number.toString().split('').length !== 4) {
+    console.log('Error: argument length must be equal 4');
+    return { message: 'Error: argument length must be equal 4' };
+  }
+
   const textOfNumber = number.toString().split('');
-  textOfNumber.length = 4;
   const arrOfNumber = textOfNumber.map((item) => +item);
 
   // Sum
@@ -60,7 +100,7 @@ function getSomeInfoOfNumber(number = 0) {
 
   // Compare part of number
   let isSumPartOfNumberEqual = false;
-  if ((arrOfNumber[0] + arrOfNumber[1]) === arrOfNumber[2] + arrOfNumber[3]) {
+  if ((arrOfNumber[0] + arrOfNumber[1]) === (arrOfNumber[2] + arrOfNumber[3])) {
     isSumPartOfNumberEqual = true;
   }
 
@@ -71,8 +111,9 @@ function getSomeInfoOfNumber(number = 0) {
   };
 }
 
-const infoOfNumber = getSomeInfoOfNumber(1234567);
+const infoOfNumber = getSomeInfoOfNumber(1234);
 console.log(infoOfNumber);
+
 
 /*
 
@@ -95,7 +136,38 @@ const nn = new Promise((res, rej) => {
   setTimeout(() => res('done'), 1000);
 });
 
-const getDataType = (obj = '') => Object.getPrototypeOf(obj).constructor.name.toLowerCase();
+const getDataType = (obj = 'empty') => {
+  if (obj === 'empty') { return 'Error: argument is not received'; }
+  if (obj === null) { return 'object'; }
+  switch (typeof (obj)) {
+    case 'number':
+      return 'number';
+    case 'boolean':
+      return 'boolean';
+    case 'string':
+      return 'string';
+    case 'undefined':
+      return 'undefined';
+    case 'function':
+      return 'function';
+    case 'object':
+      return Object.getPrototypeOf(obj).constructor.name.toLowerCase();
+    default:
+      return "I don't know what wrong happend";
+  }
+};
 
 const type = getDataType(nn);
-console.log(type);
+console.log({ type });
+
+
+// For test without error in the Browser
+try {
+  module.exports = {
+    repeatString,
+    checkIfStartsWithIf,
+    checkLastDigit,
+    getSomeInfoOfNumber,
+    getDataType,
+  };
+} catch (e) {};
