64ad3f66fb7bd9f33d3450f0e5a1c6a1422a8761
Yuriy <ivanivyura@gmail.com>
2019-11-04 22:04:46 +0200

    hw-2

--- hw2/hw-Ivaniv.js
+++ hw2/hw-Ivaniv.js
@@ -1,14 +1,64 @@
+
+
+
+
+
 //task 2
-function sumArrElements() {
-    let arr =[12, 34, 23, 56];
-    for(let i=0; i<=arr.length; i++) {
-        let sum += arr[i];
-    }
-    return sum;
+
+var arr = [12, 34, 23, 56];
+
+function sumArrElements(arr) {
+    return arr.reduce(function (base, curent) {
+        return base + curent;
+    });
 }
+
+console.warn(sumArrElements(arr));
 
 //task 4
 function factorial(x) {
- if (x <= 1) return 1;
- return x * factorial(x - 1);
+    return x <= 1 ? x : x * factorial(x - 1);
 }
+
+console.warn(factorial(5));
+
+function factorialLoop(x) {
+    if (x == 1) return x;
+    var result = 0;
+
+    for (var i = 0; i <= x; i++) {
+        result += x * (x - 1);
+    }
+
+    return result;
+}
+
+console.warn(factorialLoop(5));
+
+var baseString = "Red, Green, Blue, White1, 3, 4, 5, 7";
+
+function format(string) {
+    var temp = string.split(',');
+
+    var numberArrayResult = [];
+    var stringArrayResult = [];
+    
+    temp.forEach(x => {
+        var resultString = '';
+
+        x.split('').forEach(c => {
+            if (c.match(/\d/) && c != ' ') numberArrayResult.push(c);
+            if (c.match(/\D/) && c != ' ') resultString += c;
+        });
+
+        if (resultString) {
+            stringArrayResult.push(resultString);
+        }
+    });
+
+    return [numberArrayResult, stringArrayResult];
+}
+
+
+
+console.warn(format(baseString));
