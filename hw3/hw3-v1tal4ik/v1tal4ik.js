a659fcb107b6624e2ebc88d2d93b5ac20b39634b
v1tal4ik <vetalgta@gmail.com>
2019-11-17 17:50:19 +0200

    add code,test  all of task

--- hw3/hw3-v1tal4ik/v1tal4ik.js
+++ hw3/hw3-v1tal4ik/v1tal4ik.js
@@ -1,6 +1,8 @@
 // 1. write a constructor function `PersonFunc` whith methods : getFullName() getAge() getFullAddress();
+// eslint-disable-next-line max-classes-per-file
 const validateObj = require('./helperFunc');
-const { SarahMay, JeremieBrown, Elizabeth } = require('./people.data');
+// const { SarahMay, JeremieBrown, Elizabeth } = require('./people.data');
+const objUsers = require('./people.data');
 
 
 const PersonFunc = function (obj) {
@@ -20,15 +22,6 @@ PersonFunc.prototype.getAge = function () {
   return `${nowYear - birthYear} years`;
 };
 
-const teacher = new PersonFunc(SarahMay);
-console.log(teacher.getFullName());
-console.log(teacher.getFullAddress());
-console.log(teacher.getAge());
-const manager = new PersonFunc(JeremieBrown);
-console.log(manager);
-const trainer = new PersonFunc(Elizabeth);
-console.log(trainer);
-
 
 // 2. write a constructor function `WorkingPersonFunc` which inherits from `PersonFunc` and has extra method: getProfessionalNameAndRank();
 const WorkingPersonFunc = function (obj) {
@@ -46,19 +39,105 @@ WorkingPersonFunc.prototype.getProfessionalNameAndRank = function () {
   return `${firstName} ${lastName}, ${title}, job experiance ${experience} years`;
 };
 
-const developer = new WorkingPersonFunc(SarahMay);
-console.log(developer.getProfessionalNameAndRank());
-
-
 // 3. write a class `PersonClass` which has the same functionality as `PersonFunc` constructor function.
+
+class PersonClass {
+  constructor(obj) {
+    const { isObjHasAllProperty, isObjPropertyTypeRight, msg } = validateObj(obj);
+    if (!isObjHasAllProperty || !isObjPropertyTypeRight) {
+      return { msg };
+    }
+    this.person = obj;
+  }
+
+  getFullName() {
+    return `${this.person.firstName} ${this.person.lastName}`;
+  }
+
+  getFullAddress() {
+    const {
+      country, city, street, house, apartment,
+    } = this.person.address;
+    return `${country}, ${city}, ${street}, ${house}/${apartment}`;
+  }
+
+  getAge() {
+    const nowYear = new Date().getFullYear();
+    const birthYear = this.person.birthDate.split('.').pop();
+    return `${nowYear - birthYear} years`;
+  }
+}
 
 
 // 4. write a class `WorkingPersonClass` which inherits from `PersonClass` and has the same functionality as `WorkingPersonFunc`.
 
+class WorkingPersonClass extends PersonClass {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(obj) {
+    super(obj);
+  }
+
+  getProfessionalNameAndRank() {
+    const { firstName, lastName, job: { title, experience } } = this.person;
+    return `${firstName} ${lastName}, ${title}, job experiance ${experience} years`;
+  }
+}
+
 
 // 5. create a few instances of each class and call the methods to check whether they work correctly. You can use objects from `people.data.js` for check.
+
+console.log('********* PersonFunc ********** ');
+Object.keys(objUsers).forEach((user) => {
+  const instanceOfPersonFunc = new PersonFunc(objUsers[`${user}`]);
+  console.log(`${user} ${instanceOfPersonFunc.msg}`);
+  if (instanceOfPersonFunc.person) {
+    console.log('* getFullName :', instanceOfPersonFunc.getFullName());
+    console.log('* getFullAddress :', instanceOfPersonFunc.getFullAddress());
+    console.log('* getAge :', instanceOfPersonFunc.getAge());
+  }
+});
+
+
+console.log('\n', '********* WorkingPersonFunc ********** ');
+Object.keys(objUsers).forEach((user) => {
+  const instanceOfWorkingPersonFunc = new WorkingPersonFunc(objUsers[`${user}`]);
+  console.log(`${user} ${instanceOfWorkingPersonFunc.msg}`);
+  if (instanceOfWorkingPersonFunc.person) {
+    console.log('* getFullName :', instanceOfWorkingPersonFunc.getFullName());
+    console.log('* getFullAddress :', instanceOfWorkingPersonFunc.getFullAddress());
+    console.log('* getAge :', instanceOfWorkingPersonFunc.getAge());
+    console.log('* getProfessionalNameAndRank :', instanceOfWorkingPersonFunc.getProfessionalNameAndRank());
+  }
+});
+
+
+console.log('\n', '********* PersonClass ********** ');
+Object.keys(objUsers).forEach((user) => {
+  const instanceOfPersonClass = new PersonClass(objUsers[`${user}`]);
+  console.log(`${user} ${instanceOfPersonClass.msg}`);
+  if (instanceOfPersonClass.person) {
+    console.log('* getFullName :', instanceOfPersonClass.getFullName());
+    console.log('* getFullAddress :', instanceOfPersonClass.getFullAddress());
+    console.log('* getAge :', instanceOfPersonClass.getAge());
+  }
+});
+
+
+console.log('\n', '********* WorkingPersonClass ********** ');
+Object.keys(objUsers).forEach((user) => {
+  const instanceOfWorkingPersonClass = new WorkingPersonClass(objUsers[`${user}`]);
+  console.log(`${user} ${instanceOfWorkingPersonClass.msg}`);
+  if (instanceOfWorkingPersonClass.person) {
+    console.log('* getFullName :', instanceOfWorkingPersonClass.getFullName());
+    console.log('* getFullAddress :', instanceOfWorkingPersonClass.getFullAddress());
+    console.log('* getAge :', instanceOfWorkingPersonClass.getAge());
+    console.log('* getProfessionalNameAndRank :', instanceOfWorkingPersonClass.getProfessionalNameAndRank());
+  }
+});
 
 module.exports = {
   PersonFunc,
   WorkingPersonFunc,
+  PersonClass,
+  WorkingPersonClass,
 };
